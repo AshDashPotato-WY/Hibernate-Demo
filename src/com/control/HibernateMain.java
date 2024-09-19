9ec2d6525e8e1d1377ca9763d760bb05d8d59ea1
AshDashPotato-WY <ashleyweiyu@yahoo.com>
2024-09-19 16:26:02 -0400

    Use HQL to do selection

--- src/com/control/HibernateMain.java
+++ src/com/control/HibernateMain.java
@@ -6,12 +6,15 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class HibernateMain {
     public static void main(String[] args) {
        try {
            Configuration configuration = new Configuration().configure();
-           configuration.addAnnotatedClass(com.to.User.class);
+           configuration.addAnnotatedClass(User.class);
            StandardServiceRegistryBuilder builder =
                    new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
            // create a SessionFactory from cfg.xml
@@ -43,11 +46,33 @@ public class HibernateMain {
 //           // delete object from the user table
 //           session.delete(u);
 
-           // Demonstrate ID auto-generation by leaving the userID field empty
-           User user1 = new User("Windsor", "Accounting", "Accountant", 27);
-           User user2 = new User("Will", "Human Resource", "Manager", 45);
-           session.save(user1);
-           session.save(user2);
+//           // Demonstrate ID auto-generation by leaving the userID field empty
+//           User user1 = new User("Alice", "Accounting", "CPA", 33);
+//           User user2 = new User("Bob", "Human Resource", "Staff", 39);
+//           User user3 = new User("Rob", "Sales", "Sales person", 35);
+//           User user4 = new User("Amy", "Web", "Front-end Developer", 27);
+//           User user5 = new User("Rebecca", "Web", "Graphic Designer", 23);
+//           User user6 = new User("Owen", "Advertisement", "Account Manager", 42);
+//           User user7 = new User("Lily", "Sales", "Sales person", 32);
+//           User user8 = new User("Emily", "Web", "Back-end Developer", 30);
+//           session.save(user1);
+//           session.save(user2);
+//           session.save(user3);
+//           session.save(user4);
+//           session.save(user5);
+//           session.save(user6);
+//           session.save(user7);
+//           session.save(user8);
+
+            // use HQL to do selection
+           String hql = "from User where department=:department order by userName desc";
+            Query query = session.createQuery(hql);
+            query.setParameter("department", "Web");
+            List<User> userList = query.list();
+            System.out.println("User details");
+            for (User user: userList) {
+                System.out.println(user);
+            }
 
            // commit will help to complete the changes in the table
            transaction.commit();
--- src/com/to/User.java
+++ src/com/to/User.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 
 @Entity
 
-@Table(name="user_table")
+@Table(name="user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // it will generate ID automatically everytime we try to insert
